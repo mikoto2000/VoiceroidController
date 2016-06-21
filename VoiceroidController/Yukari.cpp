d1734b08ff781b290261aeee5a604bc0c5f186ae
mikoto2000 <mikoto2000@gmail.com>
2016-06-21 23:12:54 +0900

    Implemented "save wav process" in Yukari.
    EX でない「結月ゆかりクラス」に音声保存機能を実装。

--- VoiceroidController/Yukari.cpp
+++ VoiceroidController/Yukari.cpp
@@ -21,8 +21,9 @@ Yukari::~Yukari()
 
 const _TCHAR* Yukari::TARGET_WIN_NAME = _T("VOICEROID＋ 結月ゆかり");
 const _TCHAR* Yukari::SAVE_DIALOG_NAME = _T("音声ファイルの保存");
-const _TCHAR* Yukari::SAVING_DIALOG_NAME = _T("音声保存");
+const _TCHAR* Yukari::SAVING_DIALOG_NAME = _T("音声の保存中");
 const _TCHAR* Yukari::PLAY_BUTTON_NAME = _T(" 再生");
+const _TCHAR* Yukari::TITLE_SAVE_CONFIRM = _T("名前を付けて保存の確認");
 
 void Yukari::echo(std::string sentence, BOOL isSync)
 {
@@ -54,7 +55,63 @@ void Yukari::echo(std::string sentence, BOOL isSync)
 
 void Yukari::save(std::string sentence, std::string outFilePath, BOOL isSync)
 {
+	SendMessage(yukari, WM_ACTIVATE, WA_ACTIVE, NULL);
+	SendMessage(yukari, WM_COMMAND, COMMAND_SELECT_ALL, NULL);
+	SendMessage(yukari, WM_CHAR, _T(' '), NULL);
 
+	// クリップボードに読み上げさせたい文章を設定
+	Clipboard::setClipboardText(sentence);
+
+	// クリップボードの内容を貼り付け
+	SendMessage(yukari, WM_ACTIVATE, WA_ACTIVE, NULL);
+	SendMessage(yukari, WM_COMMAND, COMMAND_PASTE, 0);
+
+	// 保存ボタンを探して押す
+	HWND saveButton = SearchSaveButton(yukari);
+	PostMessage(saveButton, NULL, 0, 0);
+
+	Sleep(WAIT_TIME);
+
+	// "音声ファイルの保存" ウィンドウを探す
+	SearchHwndParam ssdp;
+	EnumWindows(Yukari::SearchSaveDialog, (LPARAM)&ssdp);
+	HWND saveDialog = ssdp.hwnd;
+
+	// ファイル名テキストエリアを探す
+	HWND saveFilePath = SearchSaveFilePath(saveDialog);
+
+	// ファイル名をテキストエリアに挿入
+	sendText(saveFilePath, outFilePath);
+
+	// "保存(S)" ボタン押下
+	HWND save_button_in_save_dialog = SearchSaveButtonInSaveDialog(saveDialog);
+	PostMessage(save_button_in_save_dialog, BM_CLICK, 0, 0);
+
+	Sleep(WAIT_TIME);
+
+	// "音声ファイルの保存" 上書き確認ダイアログを探す
+	SearchHwndParam ssdp2;
+	EnumWindows(Yukari::SearchConfirmationOverwriteDialog, (LPARAM)&ssdp2);
+	HWND confirmationOverwriteDialog = ssdp2.hwnd;
+
+	if (confirmationOverwriteDialog != NULL) {
+		HWND yes_button_in_confirmation_overwrite_dialog = SearchYesButtonInConfirmationOverwriteDialog(confirmationOverwriteDialog);
+		PostMessage(yes_button_in_confirmation_overwrite_dialog, BM_CLICK, 0, 0);
+	}
+
+	// 音声保存中ダイアログが消えるまで待機
+	// TODO: タイムアウト入れるか検討する
+	while (1) {
+		Sleep(END_PLAY_CHECK_INTERVAL);
+
+		SearchHwndParam ssdp;
+		ssdp.hwnd = NULL;
+		EnumWindows(SearchSavingDialog, (LPARAM)&ssdp);
+
+		if (ssdp.hwnd == NULL) {
+			break;
+		}
+	}
 }
 
 BOOL CALLBACK Yukari::SearchYukari(HWND hwnd, LPARAM lp) {
@@ -70,6 +127,55 @@ BOOL CALLBACK Yukari::SearchYukari(HWND hwnd, LPARAM lp) {
 	}
 
 	syep->hwnd = NULL;
+	return true;
+}
+
+BOOL CALLBACK Yukari::SearchSaveDialog(HWND hwnd, LPARAM lp) {
+	SearchHwndParam* ssdp = (SearchHwndParam*)lp;
+
+	TCHAR strWindowText[1024];
+	GetWindowText(hwnd, strWindowText, 1024);
+
+	if (_tcscmp(SAVE_DIALOG_NAME, strWindowText) == 0)
+	{
+		ssdp->hwnd = hwnd;
+		return false;
+	}
+
+	ssdp->hwnd = NULL;
+	return true;
+}
+
+BOOL CALLBACK Yukari::SearchSavingDialog(HWND hwnd, LPARAM lp) {
+	SearchHwndParam* ssdp = (SearchHwndParam*)lp;
+
+	TCHAR strWindowText[1024];
+	GetWindowText(hwnd, strWindowText, 1024);
+
+	if (_tcscmp(SAVING_DIALOG_NAME, strWindowText) == 0)
+	{
+		_tprintf(_T("%s が見つかりました。\n"), strWindowText);
+		ssdp->hwnd = hwnd;
+		return false;
+	}
+
+	ssdp->hwnd = NULL;
+	return true;
+}
+
+BOOL CALLBACK Yukari::SearchConfirmationOverwriteDialog(HWND hwnd, LPARAM lp) {
+	SearchHwndParam* ssdp = (SearchHwndParam*)lp;
+
+	TCHAR strWindowText[1024];
+	GetWindowText(hwnd, strWindowText, 1024);
+
+	if (_tcscmp(TITLE_SAVE_CONFIRM, strWindowText) == 0)
+	{
+		ssdp->hwnd = hwnd;
+		return false;
+	}
+
+	ssdp->hwnd = NULL;
 	return true;
 }
 
@@ -97,3 +203,47 @@ HWND CALLBACK Yukari::SearchPlayButton(HWND mainWindow) {
 
 	return tmp;
 }
+
+HWND CALLBACK Yukari::SearchSaveButton(HWND mainWindow) {
+	HWND tmp = GetWindow(mainWindow, GW_CHILD);
+	tmp = GetWindow(tmp, GW_CHILD);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_CHILD);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+
+	return tmp;
+}
+
+HWND CALLBACK Yukari::SearchSaveFilePath(HWND saveDialog) {
+	HWND tmp = GetWindow(saveDialog, GW_CHILD);
+	tmp = GetWindow(tmp, GW_CHILD);
+	tmp = GetWindow(tmp, GW_CHILD);
+	tmp = GetWindow(tmp, GW_CHILD);
+	tmp = GetWindow(tmp, GW_CHILD);
+
+	return tmp;
+}
+
+HWND CALLBACK Yukari::SearchSaveButtonInSaveDialog(HWND saveDialog) {
+	HWND tmp = GetWindow(saveDialog, GW_CHILD);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+
+	return tmp;
+}
+
+HWND CALLBACK Yukari::SearchYesButtonInConfirmationOverwriteDialog(HWND confirmationOverwriteDialog) {
+	HWND tmp = GetWindow(confirmationOverwriteDialog, GW_CHILD);
+	tmp = GetWindow(tmp, GW_CHILD);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_HWNDNEXT);
+	tmp = GetWindow(tmp, GW_CHILD);
+
+	return tmp;
+}
+
--- VoiceroidController/Yukari.h
+++ VoiceroidController/Yukari.h
@@ -32,16 +32,40 @@ private:
 	// 再生ボタン名
 	static const _TCHAR* PLAY_BUTTON_NAME;
 
+	// 名前を付けて保存の確認ダイアログ名
+	static const _TCHAR* TITLE_SAVE_CONFIRM;
+
 	// メインウィンドウのウィンドハンドル
 	HWND yukari;
 
 	// "VOICEROID＋ 結月ゆかり" ウィンドウを探す
 	static BOOL CALLBACK SearchYukari(HWND hwnd, LPARAM lp);
 
+	// "音声ファイルの保存" ダイアログを探す
+	static BOOL CALLBACK SearchSaveDialog(HWND hwnd, LPARAM lp);
+
+	// "音声保存" ダイアログを探す
+	static BOOL CALLBACK SearchSavingDialog(HWND hwnd, LPARAM lp);
+
+	// 名前を付けて保存の上書き確認ダイアログを探す
+	static BOOL CALLBACK Yukari::SearchConfirmationOverwriteDialog(HWND hwnd, LPARAM lp);
+
 	// "テキストエリア" を探す
 	static HWND CALLBACK SearchTextArea(HWND mainWindow);
 
 	// "再生ボタン" を探す
 	static HWND CALLBACK SearchPlayButton(HWND mainWindow);
+
+	// "保存ボタン" を探す
+	HWND CALLBACK Yukari::SearchSaveButton(HWND mainWindow);
+
+	// "ファイル名:" を探す
+	static HWND CALLBACK SearchSaveFilePath(HWND saveDialog);
+
+	// "保存(S)" を探す
+	static HWND CALLBACK SearchSaveButtonInSaveDialog(HWND saveDialog);
+
+	// 上書き確認ダイアログ内の「はい」ボタンを探す
+	static HWND CALLBACK SearchYesButtonInConfirmationOverwriteDialog(HWND confirmationOverwriteDialog);
 };
 
